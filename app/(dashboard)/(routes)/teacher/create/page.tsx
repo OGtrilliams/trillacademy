e278a8c34e1b275399dc42b46bb80acde2361518
OGtrilliams <tribecca@tribecc.us>
2025-10-02 21:40:23 -0400

    swapped mysql for mongodb

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -35,10 +35,15 @@ const CreatePage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("/api/course", values);
+      const response = await axios.post("/api/courses", values);
+
       router.push(`/teacher/courses/${response.data.id}`);
+
+      toast.success("Course created.");
     } catch (error) {
-      toast.error("Something went wrong");
+      console.error(error);
+
+      toast.error(`Server responded with error`);
     }
   };
   return (
